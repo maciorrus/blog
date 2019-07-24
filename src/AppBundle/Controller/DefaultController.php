37382a8b7cf512653d83b22d24a0a065dca5b6da
Maciej Michalak <m.michalak@itkontrakt.com.pl>
2019-07-24 09:16:27 +0200

    Mail works

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -18,4 +19,24 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
     }
+        /**
+         * @Route("/mail", name="mail")
+         */
+        public function mailAction(Request $request)
+        {
+            $mailer = $this->get('mailer');
+
+            try{
+                $message = \Swift_Message::newInstance()
+                    ->setFrom('maciorqus@gmail.com')
+                    ->setTo('m.michalak@itkontrakt.com.pl')
+                    ->setSubject('Raport portfela')
+                    ->setBody('W załączniku znajduje się raport portfela akwizycujnego wg doradców');
+
+                $mailer->send($message);
+            }catch (Exception $e) {
+                vardump($e);
+            }
+            return new Response('OK');
+        }
 }
